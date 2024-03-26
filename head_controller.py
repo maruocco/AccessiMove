f191710c3963d0610f9b737a02f8ee7b6093a44d
Jack <jduggan@quinnipiac.edu>
2024-03-26 06:34:58 -0400

    Added default values to thresholds

--- head_controller.py
+++ head_controller.py
@@ -19,7 +19,6 @@ class HeadController:
                        int(self.screen_height / 2) - 1)
         self.prev_x, self.prev_y = pyautogui.position()
         self.settings_menu = settings_menu
-        self.thresholds = thresholds
         self.load_threshold_values()
         self.nod_distance = 0
         self.left_distance = 0
@@ -27,28 +26,22 @@ class HeadController:
         self.press_performed = False
 
     def load_threshold_values(self):
-        # Read threshold values from the file
-        threshold_values = {}
-        with open("thresholds.txt", 'r') as file:
-            for line in file:
-                key, value = line.strip().split('=')
-                threshold_values[key.strip()] = float(value.strip())  # Convert values to float
-        # Assign threshold values to corresponding attributes
-        self.up_thesh = threshold_values.get("up_threshold")
-        self.down_thresh = threshold_values.get("down_threshold")
-        self.left_thresh = threshold_values.get("left_threshold")
-        self.right_thresh = threshold_values.get("right_threshold")
-        
-
-    """def load_threshold_values(self):
-        file_path = "thresholds.txt"
-        thresholds = self.settings_menu.load_threshold_values(file_path)
-        self.up_thresh = thresholds.get("upTiltThreshold", 60) / 100  # Divide by 100 to convert to decimal
-        self.down_thresh = thresholds.get("downTiltThreshold", 60) / 100
-        self.left_thresh = thresholds.get("leftTiltThreshold", 60) / 100
-        self.right_thresh = thresholds.get("rightTiltThreshold", 60) / 100
-        """
-        
+        try:
+            thresholds = {}
+            with open('thresholds.txt', 'r') as file:
+                for line in file:
+                    key, value = line.strip().split('=')
+                    thresholds[key.strip()] = float(value.strip())
+            self.left_thresh = thresholds.get("left_thresh", 0.14)
+            self.right_thresh = thresholds.get("right_thresh", 0.14)
+            self.up_thresh = thresholds.get("up_thresh", 0.0)
+            self.down_thresh = thresholds.get("down_thresh", 0.0)
+        except FileNotFoundError:
+            print("Error: thresholds.txt file not found.")
+        except ValueError:
+            print("Error: Invalid value in thresholds.txt.")
+        except Exception as e:
+            print("Error loading threshold values:", e)
 
     async def detect_head_tilt(self, landmarks, head_track_flag):
         left_shoulder = landmarks[mp.solutions.pose.PoseLandmark.LEFT_SHOULDER.value].x
@@ -103,11 +96,11 @@ class HeadController:
         pyautogui.click(x, y)
         win32api.SetCursorPos((self.prev_x, self.prev_y))
 
-    #def press_arrow_key(self, key):
-        #if not self.press_performed:
-            #win32api.keybd_event(key, 0, 0, 0)
-            #win32api.keybd_event(key, 0, win32con.KEYEVENTF_KEYUP, 0)
-        #self.press_performed = True
+    def press_arrow_key(self, key):
+        if not self.press_performed:
+            win32api.keybd_event(key, 0, 0, 0)
+            win32api.keybd_event(key, 0, win32con.KEYEVENTF_KEYUP, 0)
+        self.press_performed = True
 
     def get_nod_distance(self, landmarks, head_track_flag):
         left_shoulder = landmarks[mp.solutions.pose.PoseLandmark.LEFT_SHOULDER.value].x
